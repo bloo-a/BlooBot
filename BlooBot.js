0adbd8388f96dedda0d7fc8d4ceb25df4aeaf813
BlooStu2 <abrough.brough232@gmail.com>
2017-08-15 03:00:19 +0300

    bugfixes, admin exclusives & broken mod exclusives

--- BlooBot.js
+++ BlooBot.js
@@ -2,20 +2,63 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
 
+const owners = [config.ownerID1, config.ownerID2];
+
+client.on("error", (e) => console.error(e));
+client.on("warn", (e) => console.warn(e));
+client.on("debug", (e) => console.info(e));
+
 client.login(config.token);
 
 client.on("ready", () => {
   console.log("I am ready!");
+  client.user.setGame("with Bloo");
+  console.log(`Ready to serve on ${client.guilds.size} servers, for ${client.users.size} users.`);
 });
 
+
+//owner commands
+client.on("message", (message) => {
+  // Exit and stop if it's not there
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+  if (!owners.includes(message.author.id)) return;
+  if (message.content.startsWith(config.prefix + "admintest")) {
+    message.channel.send("hi admin");
+  }
+  //commands start here
+
+});
+
+//role commands
+client.on("message", (message) => {
+  // Exit and stop if it's not there
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+  if(!message.member.roles.has(config.role)) return;
+  if (message.content.startsWith(config.prefix + "modtest")) {
+    message.channel.send("hi daddy");
+  }
+  //commands start here
+
+});
+
+
+
+//global commands
 client.on("message", (message) => {
   // Exit and stop if it's not there
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
+  //commands start here
   if (message.content.startsWith(config.prefix + "ping")) {
     message.channel.send("pong!");
-  } else
+  }
   if (message.content.startsWith(config.prefix + "test")) {
     message.channel.send("shut the fuck up");
   }
+});
+
+
+client.on("guildMemberAdd", (member) => {
+  console.log(`New User "${member.user.username}" has joined "${member.guild.name}"` );
+  member.guild.defaultChannel.send(`Welcome "${member.user.username}"!`);
 });
