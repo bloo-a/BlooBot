1fc0af5e17240399aa4609c3366a10c9352f5fc3
BlooStu2 <abrough.brough232@gmail.com>
2017-12-09 17:07:21 +0300

    wawawa

--- BlooBot.js
+++ BlooBot.js
@@ -9,17 +9,18 @@ const pg = require("pg");
 
 let music = {};
 
-client.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});
+//
+// client.get('/db', function (request, response) {
+//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//     client.query('SELECT * FROM test_table', function(err, result) {
+//       done();
+//       if (err)
+//        { console.error(err); response.send("Error " + err); }
+//       else
+//        { response.render('pages/db', {results: result.rows} ); }
+//     });
+//   });
+// });
 
 fs.readdir("./events/", (err, files) => {
   if (err) return console.error(err);
