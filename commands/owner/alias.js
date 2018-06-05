74e79f916d5973b26ad9d1804a23dd658f23dd3c
BlooStu2 <abrough.brough232@gmail.com>
2018-06-05 05:13:11 +0300

    improvements to alias command

--- commands/owner/alias.js
+++ commands/owner/alias.js
@@ -4,7 +4,6 @@ const Enmap = require('enmap');
 const EnmapLevel = require('enmap-level');
 
 exports.run = (client, message, args) => {
-  let newargs = args.join(" ").split(";");
   if (message.content == "~alias"){
     const embed = new Discord.RichEmbed()
       .setTitle("Alias")
@@ -17,9 +16,28 @@ exports.run = (client, message, args) => {
     message.channel.send({embed});
   }
   else {
-    let key = newargs[0];
-    let response = newargs[1];
-    client.alias.set(key, response);
-    message.channel.send(`Alias \`${key}\` has been created`);
+    if (args[0] == "add"){
+      let newargs = args.slice(1).join(" ").split(";");
+      let key = newargs[0];
+      let response = newargs[1];
+      client.alias.set(key, response);
+      message.channel.send(`Alias \`${key}\` has been created`);
+    }
+    if (args[0] == "del" || args[0] == "delete" || args[0] == "remove" || args[0] == "rem"){
+      let key = args[1];
+      client.alias.delete(key);
+      message.channel.send(`Alias \`${key}\` has been deleted`);
+    }
+    if (args[0] == "list"){
+      var keys = client.alias.keyArray();
+      var responses = client.alias.array();
+      let list = `\`\`\`\n`;
+      for (var i = 0; i < keys.length; i++) {
+        list += `${keys[i]} : ${responses[i]}\n`;
+      }
+      list += `\`\`\``;
+      message.channel.send(`List of __**${keys.length}**__ aliases`);
+      message.channel.send(list);
+    }
   }
 };
